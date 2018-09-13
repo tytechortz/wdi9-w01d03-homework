3afa63a217ce9592f5542d6eb1eee1a76648559f
James <tytechortz@gmail.com>
2018-09-12 22:33:11 -0600

    Getting to Know You answered

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ for(let i = 0; i < 20; i++) {
 */
 
 //4. Fizz Buzz
-
+/*
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');  
@@ -46,5 +46,41 @@ for (let i = 1; i <= 100; i++) {
      } else 
      console.log(i);
     }
-        
-    
+  */
+ 
+//5. Getting to Know You
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//6 Getting to Know You
+//1.
+shahzad.splice(0, 1,'Gameboy');
+console.log(shahzad);
+
+//2.
+ageDaniel = daniel[1];
+const newAgeDaniel = (ageDaniel + 1);
+daniel.splice(1, 1, newAgeDaniel);
+console.log(daniel);
+
+//3.
+ryan.splice(2, 1, 'Gotham City');
+console.log(ryan);
+
+//4.
+reuben.pop();
+reuben.push("Chicago");
+console.log(reuben);
+
+//5.
+jim.splice(2, 1, "Moscow", "Havana", "Rancho Cucamomga");
+console.log(jim);
+
+//6.
+jim.splice(2, 1);
+console.log(jim);
+
