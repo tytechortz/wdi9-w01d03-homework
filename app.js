bc47eb81cc2cb7396988615d6b919209c0bd8123
James <tytechortz@gmail.com>
2018-09-12 18:36:47 -0600

    Commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ for(let i = 1; i <= 20; i++) {
 */
 
 //2. Get Even
-
+/*
 for (let i = 0; i <=200; i+=2) {
     console.log(i);
 
 }
+*/
+
+//3. Excited Kitten
+
+for(let i = 0; i < 20; i++) {
+    console.log('Love me, pet me! HSSSSSS!');
+    if (i % 2 === 0) {
+  let ranNum = Math.floor(Math.random() * 3);
+  //console.log(ranNum);
+    if (ranNum === 0) {
+        console.log("...human...why you taking pictures of me?...");
+          }  else if (
+        ranNum === 1) {
+            console.log("...the catnip made me do it...");
+        } else if 
+        (ranNum === 2) {
+            console.log("...why does the red dot always get away...");
+        }
+        }
+    }
