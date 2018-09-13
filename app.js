4d861539a706fb117b60d73abf1627784f7b98f0
James <tytechortz@gmail.com>
2018-09-13 03:52:21 -0600

    Commit 9-Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -136,3 +136,12 @@ console.log(kristynsCloset);
 */
 
 //Dirty Laundey and Inventory
+
+//Multiples of 3 and 5
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+   sum += i;
+}
+}     
+console.log(sum);
